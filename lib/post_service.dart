fc3b16556ab36088ff72d5636db0b1057944ca0f
Alex Vasiliev <alex@mdi-toys.ru>
2019-12-16 10:54:59 +0300

    HTTP exception

--- lib/post_service.dart
+++ lib/post_service.dart
@@ -7,9 +7,9 @@ class FakeHttpClient {
   Future<String> getResponseBody() async {
     await Future.delayed(Duration(milliseconds: 500));
     //! No Internet Connection
-    throw SocketException('No Internet');
+    // throw SocketException('No Internet');
     //! 404
-    // throw HttpException('404');
+    throw HttpException('404');
     //! Invalid JSON (throws FormatException)
     // return 'abcd';
     return '{"userId":1,"id":1,"title":"nice title","body":"cool body"}';
